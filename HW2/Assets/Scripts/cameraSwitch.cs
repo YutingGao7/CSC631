5126559eb6492a8de5e18edb89299127241ac8ee
Yuting Gao <0711candy@gmail.com>
2022-02-16 03:07:20 -0800

    final touch-up

--- HW2/Assets/Scripts/cameraSwitch.cs
+++ HW2/Assets/Scripts/cameraSwitch.cs
@@ -5,6 +5,7 @@ public class cameraSwitch : MonoBehaviour
 
     public GameObject cameraOne;
     public GameObject cameraTwo;
+    public GameObject cameraThree;
 
     // Update is called once per frame
     void Update()
@@ -13,12 +14,21 @@ public class cameraSwitch : MonoBehaviour
         {
             cameraOne.SetActive(true);
             cameraTwo.SetActive(false);
+            cameraThree.SetActive(false);
         }
 
         if (Input.GetKey("2"))
         {
             cameraOne.SetActive(false);
             cameraTwo.SetActive(true);
+            cameraThree.SetActive(false);
+        }
+
+        if (Input.GetKey("3"))
+        {
+            cameraOne.SetActive(false);
+            cameraTwo.SetActive(false);
+            cameraThree.SetActive(true);
         }
     }
 }
